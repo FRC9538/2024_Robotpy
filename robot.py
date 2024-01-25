cd6506fab9a54fba85a3d44acf787c409cab774d
RelativelyKeen <71721535+RelativelyKeen@users.noreply.github.com>
2024-01-25 14:22:22 -0800

    Multiple drivemodes and updated robotpy

--- robot.py
+++ robot.py
@@ -1,9 +1,11 @@
+from venv import logger
 import wpilib
 import wpilib.drive
 from rev import CANSparkMax
 # for some damn reason rev 2024.2.0 crashes on CANSparkMax init
 # rev is 2024.0.0b1.post1 rn
-from wpimath.geometry import Rotation2d
+
+COMPETITION = False
 
 class MyRobot(wpilib.TimedRobot):
     
@@ -13,10 +15,10 @@ class MyRobot(wpilib.TimedRobot):
         self.r_drive_lead   = CANSparkMax(2, CANSparkMax.MotorType.kBrushed)
         self.r_drive_follow = CANSparkMax(4, CANSparkMax.MotorType.kBrushed)
 
-        self.l_drive = wpilib.MotorControllerGroup(self.l_drive_lead,self.l_drive_follow)
-        self.r_drive = wpilib.MotorControllerGroup(self.r_drive_lead, self.r_drive_follow)
-        
         self.robot_drive = wpilib.drive.DifferentialDrive(self.l_drive_lead, self.r_drive_lead)
+
+        self.drive_mode = 0
+        self.drive_speed = 1
 
         # For carninal plane driving, can not get it work. SparkMax error
         # self.robot_drive = wpilib.drive.MecanumDrive(self.l_drive_lead, self.l_drive_follow, self.r_drive_lead,self.r_drive_follow)
@@ -30,23 +32,19 @@ class MyRobot(wpilib.TimedRobot):
         self.r_drive_lead.restoreFactoryDefaults(False)
         self.r_drive_follow.restoreFactoryDefaults(False)
         
-        # self.l_drive_follow.follow(self.l_drive_lead)
-        # self.r_drive_follow.follow(self.r_drive_lead)
-        
+        self.l_drive_follow.follow(self.l_drive_lead)
+        self.r_drive_follow.follow(self.r_drive_lead)
         
 
     def teleopPeriodic(self):
-
-        #Wokring drive
-        self.robot_drive.arcadeDrive(-self.controller.getRightX() * 0.7, (self.controller.getRightTriggerAxis()-self.controller.getLeftTriggerAxis()) * 0.7)
-
-        # # Bassically arcade drive with an additional button that will turn Bueford
-        # self.robot_drive.curvatureDrive(-self.controller.getRightX(), -self.controller.getRightY(), self.controller.getAButton())
-
-
-        # # For carndinal driving, does not function with DifferentialDriving. 
-        # # We need to use MecanumDriving, this has problems with SparkMax
-        # # Drive using the X, Y, and Z axes of the joystick.
-        # self.robot_drive.driveCartesian(-self.controller.getRightY(), -self.controller.getRightX(), -self.controller.getZ())
-        # # Drive at 45 degrees relative to the robot, at the speed given by the Y axis of the joystick, with no rotation.
-        # self.robot_drive.drivePolar(-self.controller.getRightY(), Rotation2d.fromDegrees(45), 0)
+        match self.drive_mode:
+            case 0:
+                self.robot_drive.arcadeDrive(-self.controller.getRightX() * self.drive_speed, -self.controller.getRightY() * self.drive_speed)
+            case 1:
+                self.robot_drive.arcadeDrive(-self.controller.getRightX() * self.drive_speed, (self.controller.getRightTriggerAxis()-self.controller.getLeftTriggerAxis()) * self.drive_speed)
+                
+        if self.controller.getStartButtonPressed() and self.controller.getRightBumper() and not COMPETITION:
+            self.drive_mode = 0 if self.drive_mode == 1 else self.drive_mode+1
+        
+        
+        # Basically arcade drive with an additional button that will turn Bueford
